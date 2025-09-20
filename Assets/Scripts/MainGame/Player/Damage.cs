4d50fcbeb4415f27032a8981574a5e50f5ee5992
ocfrey <ocfrey270@gmail.com>
2025-09-20 11:43:55 -0600

    Update Damage.cs

--- Assets/Scripts/MainGame/Player/Damage.cs
+++ Assets/Scripts/MainGame/Player/Damage.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class damage : MonoBehaviour
@@ -5,14 +6,39 @@ public class damage : MonoBehaviour
     public int health = 10;
     public PolygonCollider2D collider;
     public SceneManager gameOver;
-    
+    public float cooldownTimer;
+    public float cooldownScale = 2f;
+    private bool cooldownActive = false;
+    private float timer;
+
+
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        health--;
-        Debug.Log(health);
-        if (health == 0)
+        if (!cooldownActive)
         {
-            SceneManager.LoadScene("GameOver");
+            health--;
+            cooldownTimer = cooldownScale;
+            cooldownActive = true;
+            Debug.Log(health);
+            if (health == 0)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
         }
     }
+
+    void Update()
+    {
+        if (cooldownActive)
+        {
+
+            cooldownTimer -= Time.deltaTime;
+            if (cooldownTimer <= 0)
+            {
+                cooldownActive = false;
+            }
+        }
+    }
+
 }
