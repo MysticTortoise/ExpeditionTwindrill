0e61e10a309a8080c5021b323224045348323130
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-09-20 13:23:02 -0600

    let me fking do this brhd

---
+++ Assets/Scripts/Menu UI/ExitButton.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using System.Collections;
+
+/// <summary>
+/// yet another overcomplicated script for exit button effects
+/// </summary>
+public class ExitButton : MonoBehaviour
+{
+    bool exitGame = false; // if true, next press exits the game
+    bool isAnimating = false;
+
+    [SerializeField] Button exitButton;
+    [SerializeField] Image promptBackground;
+    [SerializeField] TextMeshProUGUI promptText; //the one that says "EXIT GAME confirm?"
+    [SerializeField] TextMeshProUGUI promptTimerText;
+    [SerializeField] TextMeshProUGUI promptTextFinal; //the one that says "EXIT GAME"
+
+    Vector3 textExpandInitSize = new Vector3(0.85f, 0.85f, 0); //final size is always 1
+    Vector3 buttonInitialSize = new Vector3(64, 64, 0);
+    Vector3 finalPromptTxtFinalSize = new Vector3(164, 50, 0);
+    [SerializeField] Vector2 buttonExpandFinalSize;
+
+    /// <summary>
+    /// brings up a confirmation dialog to exit the game
+    /// </summary>
+    public void OnClick()
+    {
+        if (exitGame != true && isAnimating != true)
+        {
+            StartCoroutine(ExitPromptSequence());
+        }
+        else if (exitGame == true)
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator ExitPromptSequence()
+    {
+        isAnimating = true;
+
+        //set initial text scale
+        promptText.transform.localScale = textExpandInitSize;
+        promptTimerText.transform.localScale = textExpandInitSize;
+        promptTextFinal.transform.localScale = textExpandInitSize;
+
+        exitButton.image.DOFade(0f, 0.3f);
+        promptBackground.DOFade(1f, 0.3f);
+        exitButton.image.rectTransform.DOSizeDelta(buttonExpandFinalSize, 0.8f).SetEase(Ease.OutExpo).OnComplete(() =>
+        {
+            promptText.transform.DOScale(1, 0.2f).SetEase(Ease.OutBack);
+            promptTimerText.transform.DOScale(1, 0.2f).SetEase(Ease.OutBack);
+            promptText.DOFade(1f, 0.1f);
+            promptTimerText.DOFade(1f, 0.1f);
+        });
+
+        //countdown & update timer
+        yield return new WaitForSeconds(3f);
+        
+
+    }
+}
