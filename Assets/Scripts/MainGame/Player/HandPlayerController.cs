8f5efe3fbf7c4a5f3f36f04117e4d0dfc3b62bd1
MysticTortoise <shadowknight7788@gmail.com>
2025-09-19 20:47:31 -0600

    arm max dist

--- Assets/Scripts/MainGame/Player/HandPlayerController.cs
+++ Assets/Scripts/MainGame/Player/HandPlayerController.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 public class HandPlayerController : MonoBehaviour
 {
 
+    [SerializeField] private float MaxArmDist;
+
     [HideInInspector] public Vector3 goalPosition;
+    private Vector3 trueGoal;
 
     private SubPlayerController sub;
     private Transform chainTransform;
 
 
     public void SetSub(SubPlayerController sub) { this.sub = sub; }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,13 +24,16 @@ public class HandPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = goalPosition;
+        Vector3 offset = (goalPosition - sub.transform.position);
+        trueGoal = sub.transform.position + offset.normalized * Mathf.Min(MaxArmDist, offset.magnitude);
 
-        chainTransform.position = sub.transform.position + ((goalPosition - sub.transform.position) / 2);
+        transform.position = trueGoal;
+
+        // Update Chain visuals
+        chainTransform.position = sub.transform.position + ((transform.position - sub.transform.position) / 2);
         chainTransform.rotation = Quaternion.Euler(0,0, 
-            Mathf.Rad2Deg * Mathf.Atan2(goalPosition.y - sub.transform.position.y, goalPosition.x - sub.transform.position.x) + 90);
-        Debug.Log(Vector3.Distance(goalPosition, sub.transform.position));
-        chainTransform.localScale = new Vector3(.5f, Vector3.Distance(goalPosition, sub.transform.position),.5f);
+            Mathf.Rad2Deg * Mathf.Atan2(transform.position.y - sub.transform.position.y, transform.position.x - sub.transform.position.x) + 90);
+        chainTransform.localScale = new Vector3(.5f, Vector3.Distance(transform.position, sub.transform.position),.5f);
     }
 
 }
