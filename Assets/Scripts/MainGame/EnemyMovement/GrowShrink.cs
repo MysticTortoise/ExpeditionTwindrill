219efbc3c9e59148a20036ff427f20d210193fe0
ocfrey <ocfrey270@gmail.com>
2025-09-19 22:19:50 -0600

    grow method

---
+++ Assets/Scripts/MainGame/EnemyMovement/GrowShrink.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+
+public class GrowShrink : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform enemy;
+    private float timer;
+    public float sizeMin;
+    public float sizeMax;
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float size = Mathf.Lerp(sizeMin, sizeMax, Math.Abs(Mathf.Sin(timer)));
+        enemy.localScale = new Vector3(size, size, size);
+    
+    }
+}
