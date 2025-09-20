3dd0e31a128b9e43da8e4c61dbb33f85b0445760
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-09-20 15:05:52 -0600

    UI Updates

--- Assets/Scripts/Menu UI/ExitButton.cs
+++ Assets/Scripts/Menu UI/ExitButton.cs
@@ -21,6 +21,7 @@ public class ExitButton : MonoBehaviour
     Vector3 textExpandInitSize = new Vector3(0.85f, 0.85f, 0); //final size is always 1
     Vector3 buttonInitialSize = new Vector3(64, 64, 0);
     Vector3 finalPromptTxtFinalSize = new Vector3(164, 50, 0);
+    [SerializeField] Vector3 finalPromptTxtInitSize = new Vector3(0.85f, 0.85f, 0);
     [SerializeField] Vector2 buttonExpandFinalSize;
 
     /// <summary>
@@ -34,6 +35,7 @@ public class ExitButton : MonoBehaviour
         }
         else if (exitGame == true)
         {
+            Debug.Log("goodbye!");
             Application.Quit();
         }
     }
@@ -58,7 +60,40 @@ public class ExitButton : MonoBehaviour
         });
 
         //countdown & update timer
-        yield return new WaitForSeconds(3f);
+        promptTimerText.text = "(2)";
+        yield return new WaitForSeconds(1);
+        promptTimerText.text = "(2)";
+        yield return new WaitForSeconds(1);
+        promptTimerText.text = "(1)";
+        yield return new WaitForSeconds(1);
+
+        //final exit animation
+        promptText.DOFade(0f, 0.1f);
+        promptTimerText.DOFade(0f, 0.1f);
+        promptTextFinal.rectTransform.DOSizeDelta(finalPromptTxtFinalSize, 0.15f).SetEase(Ease.OutBack);
+        promptTextFinal.DOFade(1f, 0.15f).OnComplete(() =>
+        {
+            exitGame = true;
+            isAnimating = false;
+        });
+
+
+        //wait 5 seconds and reset if player doesn't exit
+        yield return new WaitForSeconds(5);
+        exitGame = false;
+        isAnimating = true;
+
+
+        promptTextFinal.rectTransform.DOSizeDelta(finalPromptTxtInitSize, 0.2f).SetEase(Ease.OutExpo);
+        promptTextFinal.DOFade(0f, 0.1f).OnComplete(() =>
+        {
+            exitButton.image.DOFade(1f, 0.3f);
+            promptBackground.DOFade(0f, 0.3f);
+            exitButton.image.rectTransform.DOSizeDelta(buttonInitialSize, 0.8f).SetEase(Ease.OutExpo).OnComplete(() =>
+            {
+                isAnimating = false;
+            });
+        });
         
 
     }
