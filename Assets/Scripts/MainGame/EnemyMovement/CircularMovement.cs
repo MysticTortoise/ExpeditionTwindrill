ae3aff18263566387aeba147c8cab8e07b493988
MysticTortoise <shadowknight7788@gmail.com>
2025-11-01 19:21:25 -0600

    eels are animated

--- Assets/Scripts/MainGame/EnemyMovement/CircularMovement.cs
+++ Assets/Scripts/MainGame/EnemyMovement/CircularMovement.cs
@@ -25,7 +25,7 @@ public class CircleMovement : MonoBehaviour
         transform.position = originPoint + new Vector3(
             Mathf.Cos(timer) * radius, Mathf.Sin(timer) * radius
             );
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(Mathf.Cos(timer), Mathf.Sin(timer)));
+        transform.rotation = Quaternion.Euler(0, 0, (float)((timer / (Math.PI*2)) * 360f) + 90);
     }
 
     private void OnDrawGizmosSelected()
