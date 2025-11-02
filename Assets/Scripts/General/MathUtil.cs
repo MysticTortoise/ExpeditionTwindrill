46042a2b229304d0cf77df6dc99a3821bbecf468
MysticTortoise <shadowknight7788@gmail.com>
2025-11-01 19:09:15 -0600

    all aspect ratios i think are supported now, redrawn one of the level 3 tutorials, and fixed the collision issue in level 3

--- Assets/Scripts/General/MathUtil.cs
+++ Assets/Scripts/General/MathUtil.cs
@@ -11,4 +11,14 @@ public class MathUtil
     {
         return 2 * (0.5f - Mathf.Abs(x % 1 - 0.5f));
     }
+    
+    public static Bounds OrthographicBounds(Camera camera)
+    {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = camera.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            camera.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
+    }
 }
--- Assets/Scripts/MainGame/Environment/ParallaxBackground.cs
+++ Assets/Scripts/MainGame/Environment/ParallaxBackground.cs
@@ -5,6 +5,9 @@ public class ParallaxBackground : MonoBehaviour
     private Material material;
     [SerializeField] private new Camera camera;
     [SerializeField] private Vector2 scrollModifier;
+
+    private const float Base43RatioMaxExtents = 13.3382f;
+    private const float Base43RatioScale = 6.993493f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,7 +21,15 @@ public class ParallaxBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = camera.transform.position + new Vector3(0,0,5);
+        Bounds bounds = MathUtil.OrthographicBounds(camera);
+        float biggestSize = Mathf.Max(bounds.extents.x, bounds.extents.y);
+        
+        
+        
+        transform.position = bounds.center + new Vector3(0,0,5);
+        float desiredSize = (Base43RatioScale / Base43RatioMaxExtents) * biggestSize;
+        transform.localScale = new Vector3(desiredSize,desiredSize,1);
         material.SetVector("_UVOffset", camera.transform.position * scrollModifier);
+        material.SetFloat("_UVScale", biggestSize / Base43RatioMaxExtents);
     }
 }
